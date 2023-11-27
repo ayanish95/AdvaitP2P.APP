5e19681404c126d1cc76c61c776b160f54d72431
ayanish95 <ayanish95@gmail.com>
2023-11-27 18:44:08 +0530

    ASN View Page Changes

--- src/app/pages/advance-shipping-notification/view-advance-shipping-notification/view-advance-shipping-notification.component.ts
+++ src/app/pages/advance-shipping-notification/view-advance-shipping-notification/view-advance-shipping-notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ResultEnum } from '@core/enums/result-enum';
@@ -6,6 +6,8 @@ import { ToastrService } from 'ngx-toastr';
 import { finalize } from 'rxjs';
 import { AdvanceShippingNotificationService } from '@core/services/advance-shipment-notification.service';
 import { AdvancedShipmentNotificationVM } from './../../../core/models/advance-shipping-notification';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonEnum } from '@core/enums/common-enum';
 
 @Component({
   selector: 'app-view-advance-shipping-notification',
@@ -22,14 +24,23 @@ export class ViewAdvanceShippingNotificationComponent {
     'DeliveryDate',
     'Plant',
     'Location',
+    'View',
     // 'Close',
     // 'RFQ',
+  ];
+
+  displayedPackingColumns: string[] = [
+    'srNo',
+    'BatchNo',
+    'SerialNo',
+    'Qty'    
   ];
   ASNId!: number;
   ASNDetails!: AdvancedShipmentNotificationVM;
   dataSource = new MatTableDataSource<any>();
+  packingdataSource = new MatTableDataSource<any>();
   index = 0;
-  constructor(private ASNService: AdvanceShippingNotificationService, private toaster: ToastrService, private route: ActivatedRoute) {
+  constructor(private ASNService: AdvanceShippingNotificationService, private toaster: ToastrService, private route: ActivatedRoute,private dialog: MatDialog) {
 
     this.route.queryParams.subscribe((params: any) => {
       this.ASNId = params.id;
@@ -56,6 +67,48 @@ export class ViewAdvanceShippingNotificationComponent {
         else
           this.toaster.error(res[ResultEnum.Message]);
       });
+  }
+
+  async openModelForViewItem(templateRef: TemplateRef<any>,data?: any) {
+    //this.selecteItemQty = 0;
+    const isSerialNo = data?.IsSerialNo;
+    const isBatchNo = data?.IsBatchNo;
+   
+    const type = this.checkProductType(isSerialNo, isBatchNo);
+    if (data?.Qty) {
+      // this.selecteItemQty = data.Qty;
+      // this.selectePOLineId = data?.POLineId;
+      // this.selectePOId = data?.POHeaderId;
+      // if (!this.selectePOId || !this.selectePOLineId)
+      //   throw this.toaster.error('PO Id or PO Line Id not found for selected row...');
+      if (type != CommonEnum.None) {
+        if (type != CommonEnum.BatchNo) {
+          for (let index = 0; index < data?.Qty; index++) {
+            //this.batchAndSerialNoGroupForm().push(this.createFormForBatchAndSerialNo(type, this.selectePOId, this.selectePOLineId));
+          }
+        }
+        else {
+          //this.batchAndSerialNoGroupForm().push(this.createFormForBatchAndSerialNo(type, this.selectePOId, this.selectePOLineId));
+        }
+      }
+    }
+
+    this.dialog.open(templateRef, {
+      width: type == CommonEnum.All ? '56vw' : '45vw',
+      panelClass: 'custom-modalbox'
+    });
+  }
+
+  checkProductType(isSerialNo: any, isBatchNo: any) {
+    if (isBatchNo && !isSerialNo)
+      return CommonEnum.BatchNo;
+    else if (!isBatchNo && isSerialNo)
+      return CommonEnum.SerialNo;
+    else if (isBatchNo && isSerialNo)
+      return CommonEnum.All;
+    else if (!isBatchNo && !isSerialNo)
+      return CommonEnum.None;
+    return CommonEnum.None;
   }
 
 }
