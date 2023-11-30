89744b47599bf9fa9957ff506d76cfc742f7ee90
himanshu-hpp <himanshu.rajput@advaitbusiness.com>
2023-11-30 13:06:56 +0530

    letest grn

--- src/app/core/services/return-goods-reception-notification.service.ts
+++ src/app/core/services/return-goods-reception-notification.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClientService } from './http-client.service';
 import { Api } from '@core/consts/api';
 import { Method } from '@core/consts/method';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
       providedIn: 'root'
@@ -14,4 +16,12 @@ export class ReturnGoodsReceptionNotificationService {
       CreateGRN(GRNVM: any) {
         return this.httpclient.authPost(`${Api.GoodsReceiptNotification + Method.CreateGR}`,GRNVM);
       }
+      GetallGrList(): Observable<any> {
+        return this.httpclient.authGet(`${Api.GoodsReceiptNotification + Method.GetAllGRList}`);
+      }
+
+  getGrnDetailsById(GrnId:number): Observable<any> {
+    return this.httpclient.authGet(`${Api.Plant + Method.GrnListById}/${GrnId}`);
+  }
+
 }
